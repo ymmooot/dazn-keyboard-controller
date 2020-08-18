be0284312753d92ce256542e0a6c2c10779955c5
ymmooot <ryota.yama.9+github@gmail.com>
2020-08-18 15:35:48 +0900

    add Readme.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# DAZN Keyboard Controller
+
+[![main](https://github.com/ymmooot/dazn-keyboard-controller/workflows/main/badge.svg)](https://github.com/ymmooot/dazn-keyboard-controller/actions)
--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/"
-  ],
-  "testMatch": [
-    "**/test/**/*.+(ts|js)",
-  ],
-  "transform": {
-    "^.+\\.ts$": "ts-jest"
+  roots: ['<rootDir>/'],
+  testMatch: ['**/test/**/*.+(ts|js)'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
   },
-  "moduleNameMapper": {
-    "^@/(.+)": "<rootDir>/src/$1"
+  moduleNameMapper: {
+    '^@/(.+)': '<rootDir>/src/$1',
   },
-  "globals": {
-    "ts-jest": {
-      "tsConfig": "tsconfig.test.json"
-    }
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
   },
-}
+};
